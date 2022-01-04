be13075cd80e234ba957d2a5bbc46754a3a0af37
ToniJakovcevic10 <toni.jakovcevic10@gmail.com>
2022-01-04 10:45:00 +0100

    playerMovement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,27 +1,33 @@
 extends KinematicBody
 
-var playerName = "player"
 var velocity = Vector3(0,0,0)
-const SPEED = 6
+const SPEED = 10
+const ballRotationSpeed = 10
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_right") and Input.is_action_just_pressed("ui_left"):
+	
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		velocity.x = 0
-	elif Input.is_action_just_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
-	elif Input.is_action_just_pressed("ui_left"):
+		$MeshInstance.rotate_z(deg2rad(-ballRotationSpeed))
+	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
+		$MeshInstance.rotate_z(deg2rad(ballRotationSpeed))
 	else:
 		velocity.x = lerp(velocity.x,0,0.1)
-	if Input.is_action_just_pressed("ui_up") and Input.is_action_just_pressed("ui_down"):
+	
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.z = 0
-	elif Input.is_action_just_pressed("ui_up"):
+	elif Input.is_action_pressed("ui_up"):
 		velocity.z = -SPEED
-	elif Input.is_action_just_pressed("ui_down"):
+		$MeshInstance.rotate_x(deg2rad(-ballRotationSpeed))
+	elif Input.is_action_pressed("ui_down"):
 		velocity.z = SPEED
+		$MeshInstance.rotate_x(deg2rad(ballRotationSpeed))
 	else:
 		velocity.z = lerp(velocity.z,0,0.1)
 	move_and_slide(velocity)
